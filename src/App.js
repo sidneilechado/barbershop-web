afab36c40e489adcf083be950c513b45ef2557c2
Sid <juniorlechado@gmail.com>
2019-12-01 05:22:20 -0300

    Added validation on forms

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import React from 'react';
 import { Router } from 'react-router-dom';
 
@@ -6,10 +8,13 @@ import '~/config/ReactotronConfig';
 import Routes from '~/routes';
 import history from '~/services/history';
 
+import GlobalStyle from '~/styles/global';
+
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,9 @@
+import 'dotenv/config';
+
 import Reactotron from 'reactotron-react-js';
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure().connect();
+  const tron = Reactotron.configure().connect({ host: '192.168.11.191' });
 
   tron.clear();
 
